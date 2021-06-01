2f7070c2f25ad0bb541ead6f5e9ed9091886cc45
pujasa14 <84658388+pujasa14@users.noreply.github.com>
2021-06-01 04:07:22 -0700

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,20 @@
+package Activities;
+
+import org.testng.annotations.Test;
+
+public class Activity8 {
+  @Test
+  public void f() {
+  
+	 /* <classes>
+	    <class name="Activities.Activity1"/>
+      <class name="Activities.Activity2"/>
+      <class name="Activities.Activity3"/>
+      <class name="Activities.Activity5"/>
+      <class name="Activities.Activity6"/>
+      
+</classes> 
+ */
+  }
+  
+}
