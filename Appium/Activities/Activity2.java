a097975617f22ff7391d680d11d5d3b67fcbbe9e
pujasa14 <84658388+pujasa14@users.noreply.github.com>
2021-06-02 04:25:41 -0700

    Add files via upload

---
+++ Appium/Activities/Activity2.java
@@ -0,0 +1,36 @@
+package Activities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class Activity2 {
+    WebDriverWait wait;
+    AppiumDriver<MobileElement> driver = null;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "<device name>");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appPackage", "com.android.chrome");
+        caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        caps.setCapability("noReset", true);
+
+        // Instantiate Appium Driver
+        driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), caps);
+        wait = new WebDriverWait(driver, 10);
+    }
